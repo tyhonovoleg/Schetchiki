4a1d887e62af50897448b4a8d714babf7468b4b7
Олег Тихонов <tyhonovoleg@gmail.com>
2020-06-27 17:47:54 +0300

    Я сделяль

---
+++ Shetchiki/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Shetchiki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(920, 212);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 385);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Shetchiki/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Shetchiki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = 11;
+            dataGridView1.Columns[0].Name = "Серийный номер радиомодуля №";
+            dataGridView1.Columns[1].Name = "Артикул прибора №";
+            dataGridView1.Columns[2].Name = "Номер радио сети";
+            dataGridView1.Columns[3].Name = "Последний день текущего месяца";
+            dataGridView1.Columns[4].Name = "Подъезд";
+            dataGridView1.Columns[5].Name = "Место установки";
+            dataGridView1.Columns[6].Name = "Квартира №";
+            dataGridView1.Columns[7].Name = "Начальные показатели прибора";
+            dataGridView1.Columns[8].Name = "Тип воды горячая или холодная";
+            dataGridView1.Columns[9].Name = "?";
+            dataGridView1.Columns[10].Name = "Серийный номер счетчика воды №";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string[] filenames = openFileDialog1.FileNames;
+            dataGridView1.RowCount = filenames.Length;
+            for (int j = 0; j < filenames.Length; j++)
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(filenames[j]);
+                XmlElement xRoot = xDoc.DocumentElement;
+                foreach (XmlNode xnode in xRoot)
+                {
+                    if (xnode.ChildNodes.Count > 0)
+                    {
+                        string[] row = new string[11];
+                        for (int i = 0; i < 11; i++)
+                        {
+                            var cell = xnode.ChildNodes[i];
+                            if (cell.Name == "W8")
+                            {
+                                row[i] = cell.InnerText == "kalt" ? "холодная" : "горячая";
+                            }
+                            else
+                            {
+                                row[i] = cell.InnerText;
+                            }
+
+                        }
+
+                        dataGridView1.Rows[j].SetValues(row);
+
+                    }
+
+                }
+            }
+
+        }
+    }
+}
