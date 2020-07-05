ef3f0bd611f31db0ab37f3828301638e302b0c6d
Олег Тихонов <tyhonovoleg@gmail.com>
2020-07-05 20:40:35 +0300

    Добавлены ещё два типа приборов, поиск.

--- Shetchiki/Form1.Designer.cs
+++ Shetchiki/Form1.Designer.cs
@@ -30,44 +30,84 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(920, 212);
+            this.dataGridView1.Size = new System.Drawing.Size(1060, 162);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 302);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 49);
+            this.button1.Size = new System.Drawing.Size(1060, 33);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Добавить файл";
+            this.button1.Text = "Выбрать папку";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // openFileDialog1
+            // dataGridView2
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Multiselect = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 245);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1060, 160);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(12, 411);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(1060, 138);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Поиск по номеру прибора";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 385);
+            this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,7 +115,10 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Shetchiki/Form1.cs
+++ Shetchiki/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,60 +14,316 @@ namespace Shetchiki
 {
     public partial class Form1 : Form
     {
+        List<string[]> table19320 = new List<string[]>();
+        List<string[]> table19450 = new List<string[]>();
+        List<string[]> table11490 = new List<string[]>();
+
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.ColumnCount = 11;
+
+            dataGridView1.ColumnCount = 12;
             dataGridView1.Columns[0].Name = "Серийный номер радиомодуля №";
             dataGridView1.Columns[1].Name = "Артикул прибора №";
             dataGridView1.Columns[2].Name = "Номер радио сети";
-            dataGridView1.Columns[3].Name = "Последний день текущего месяца";
+            dataGridView1.Columns[3].Name = "Дата архивации показаний";
             dataGridView1.Columns[4].Name = "Подъезд";
             dataGridView1.Columns[5].Name = "Место установки";
             dataGridView1.Columns[6].Name = "Квартира №";
             dataGridView1.Columns[7].Name = "Начальные показатели прибора";
             dataGridView1.Columns[8].Name = "Тип воды горячая или холодная";
-            dataGridView1.Columns[9].Name = "?";
+            dataGridView1.Columns[9].Name = "Вес импульса";
             dataGridView1.Columns[10].Name = "Серийный номер счетчика воды №";
+            dataGridView1.Columns[11].Name = "Папка";
+
+            dataGridView2.ColumnCount = 10;
+            dataGridView2.Columns[0].Name = "Серийный номер радиомодуля №";
+            dataGridView2.Columns[1].Name = "Артикул радиомодуля №";
+            dataGridView2.Columns[2].Name = "Номер радио сети";
+            dataGridView2.Columns[3].Name = "Дата архивации показаний";
+            dataGridView2.Columns[4].Name = "Подъезд";
+            dataGridView2.Columns[5].Name = "Место установки";
+            dataGridView2.Columns[6].Name = "Квартира №";
+            dataGridView2.Columns[7].Name = "Начальные показатели прибора";
+            dataGridView2.Columns[8].Name = "Серийный номер теплосчетчика";
+            dataGridView2.Columns[9].Name = "Папка";
+
+            dataGridView3.ColumnCount = 12;
+            dataGridView3.Columns[0].Name = "Номер распределителя";
+            dataGridView3.Columns[1].Name = "Артикул прибора №";
+            dataGridView3.Columns[2].Name = "Номер радио сети";
+            dataGridView3.Columns[3].Name = "Дата архивации показаний";
+            dataGridView3.Columns[4].Name = "Подъезд";
+            dataGridView3.Columns[5].Name = "Место установки";
+            dataGridView3.Columns[6].Name = "Квартира №";
+            dataGridView3.Columns[7].Name = "Дата монтажа";
+            dataGridView3.Columns[8].Name = "Тип прибора";
+            dataGridView3.Columns[9].Name = "Температура";
+            dataGridView3.Columns[10].Name = "Тип учета";
+            dataGridView3.Columns[11].Name = "Папка";
+        }
+        private List<string> GetFiles(string path)
+        {
+            List<string> result = new List<string>();
+            List<string> files = Directory.GetFiles(path).ToList();
+            List<string> folders = Directory.GetDirectories(path).ToList();
+            foreach (string file in files)
+            {
+                result.Add(file);
+            }
+            foreach (string folder in folders)
+            {
+                List<string> list = GetFiles(folder);
+                foreach (string file in list)
+                {
+                    result.Add(file);
+                }
+            }
+            return result;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
-                return;
-            string[] filenames = openFileDialog1.FileNames;
-            dataGridView1.RowCount = filenames.Length;
-            for (int j = 0; j < filenames.Length; j++)
+            using (var fbd = new FolderBrowserDialog())
             {
-                XmlDocument xDoc = new XmlDocument();
-                xDoc.Load(filenames[j]);
-                XmlElement xRoot = xDoc.DocumentElement;
-                foreach (XmlNode xnode in xRoot)
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
                 {
-                    if (xnode.ChildNodes.Count > 0)
+                    List<string> files = GetFiles(fbd.SelectedPath);
+
+                    List<string> files19320 = new List<string>();
+                    List<string> files19450 = new List<string>();
+                    List<string> files11490 = new List<string>();
+  
+                    foreach (string file in files)
                     {
-                        string[] row = new string[11];
-                        for (int i = 0; i < 11; i++)
+                        string[] parts = file.Split('\\');
+                        string filename = parts[parts.Length - 1];
+                        if (filename.EndsWith(".xml"))
                         {
-                            var cell = xnode.ChildNodes[i];
-                            if (cell.Name == "W8")
+                            if (filename.StartsWith("19320"))
                             {
-                                row[i] = cell.InnerText == "kalt" ? "холодная" : "горячая";
+                                files19320.Add(file);
                             }
-                            else
+                            if (filename.StartsWith("19450"))
                             {
-                                row[i] = cell.InnerText;
+                                files19450.Add(file);
+                            }
+                            if (filename.StartsWith("11490"))
+                            {
+                                files11490.Add(file);
+                            }
+                        }
+                    }
+
+                   
+                    dataGridView1.RowCount = files19320.Count;
+                    for (int j = 0; j < files19320.Count; j++)
+                    {
+                        string filename = files19320[j];
+                        XmlDocument xDoc = new XmlDocument();
+                        xDoc.Load(filename);
+                        XmlElement xRoot = xDoc.DocumentElement;
+                        foreach (XmlNode xnode in xRoot)
+                        {
+                            if (xnode.ChildNodes.Count > 0)
+                            {
+                                string[] row = new string[dataGridView1.ColumnCount];
+                                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                                {
+                                    var cell = xnode.ChildNodes[i];
+                                    if (cell.Name == "W8")
+                                    {
+                                        row[i] = cell.InnerText == "kalt" ? "холодная" : "горячая";
+                                    }
+                                    else
+                                    {
+                                        row[i] = cell.InnerText;
+                                    }
+
+                                    if (i == 11)
+                                    {
+                                        if (filename.Contains("inbetriebnahme"))
+                                        {
+                                            row[i] = "inbetriebnahme";
+                                        }
+                                        else if (filename.Contains("reparametrierung"))
+                                        {
+                                            row[i] = "reparametrierung";
+                                        }
+                                        else if (filename.Contains("austausch"))
+                                        {
+                                            row[i] = "austausch";
+                                        }
+
+                                    }
+                                }
+
+                                table19320.Add(row);
+                                dataGridView1.Rows[j].SetValues(row);
+
                             }
 
                         }
-
-                        dataGridView1.Rows[j].SetValues(row);
-
                     }
 
+                    dataGridView2.RowCount = files19450.Count;
+                    for (int j = 0; j < files19450.Count; j++)
+                    {
+                        string filename = files19450[j];
+                        XmlDocument xDoc = new XmlDocument();
+                        xDoc.Load(filename);
+                        XmlElement xRoot = xDoc.DocumentElement;
+                        foreach (XmlNode xnode in xRoot)
+                        {
+                            if (xnode.ChildNodes.Count > 0)
+                            {
+                                string[] row = new string[dataGridView2.ColumnCount];
+                                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                                {
+                                    var cell = xnode.ChildNodes[i];
+                                    if (cell != null && cell.Name != "Property")
+                                    {
+                                        row[i] = cell.InnerText;
+                                    }
+                                    if (i == 9)
+                                    {
+                                        if (filename.Contains("inbetriebnahme"))
+                                        {
+                                            row[i] = "inbetriebnahme";
+                                        }
+                                        else if (filename.Contains("reparametrierung"))
+                                        {
+                                            row[i] = "reparametrierung";
+                                        }
+                                        else if (filename.Contains("austausch"))
+                                        {
+                                            row[i] = "austausch";
+                                        }
+
+                                    }
+
+                                }
+
+                                table19450.Add(row);
+                                dataGridView2.Rows[j].SetValues(row);
+
+                            }
+
+                        }
+                    }
+
+                    dataGridView3.RowCount = files11490.Count;
+                    for (int j = 0; j < files11490.Count; j++)
+                    {
+                        string filename = files11490[j];
+                        XmlDocument xDoc = new XmlDocument();
+                        xDoc.Load(filename);
+                        XmlElement xRoot = xDoc.DocumentElement;
+                        foreach (XmlNode xnode in xRoot)
+                        {
+                            if (xnode.ChildNodes.Count > 0)
+                            {
+                                string[] row = new string[dataGridView3.ColumnCount];
+                                for (int i = 0; i < dataGridView3.ColumnCount; i++)
+                                {
+                                    var cell = xnode.ChildNodes[i];
+                                    row[i] = cell.InnerText;
+                                    if (i == 11)
+                                    {
+                                        if (filename.Contains("inbetriebnahme"))
+                                        {
+                                            row[i] = "inbetriebnahme";
+                                        }
+                                        else if (filename.Contains("reparametrierung"))
+                                        {
+                                            row[i] = "reparametrierung";
+                                        }
+                                        else if (filename.Contains("austausch"))
+                                        {
+                                            row[i] = "austausch";
+                                        }
+
+                                    }
+
+                                }
+
+
+                                table11490.Add(row);
+                                dataGridView3.Rows[j].SetValues(row);
+
+                            }
+
+                        }
+                    }
                 }
             }
 
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string search = textBox1.Text.Trim();
+
+            List<string[]> filteredTable19320 = new List<string[]>();
+            foreach (string[] row in table19320)
+            {
+                if (row[0].Contains(search))
+                {
+                    filteredTable19320.Add(row);
+                }
+            }
+            dataGridView1.Rows.Clear();
+            if (filteredTable19320.Count > 0)
+            {
+                dataGridView1.RowCount = filteredTable19320.Count;
+                for (int i = 0; i < filteredTable19320.Count; i++)
+                {
+                    string[] row = filteredTable19320[i];
+                    dataGridView1.Rows[i].SetValues(row);
+                }
+            }
+
+            List<string[]> filteredTable19450 = new List<string[]>();
+            foreach (string[] row in table19450)
+            {
+                if (row[0].Contains(search))
+                {
+                    filteredTable19450.Add(row);
+                }
+            }
+            dataGridView2.Rows.Clear();
+            if (filteredTable19450.Count > 0)
+            {
+                dataGridView2.RowCount = filteredTable19450.Count;
+                for (int i = 0; i < filteredTable19450.Count; i++)
+                {
+                    string[] row = filteredTable19450[i];
+                    dataGridView2.Rows[i].SetValues(row);
+                }
+            }
+
+            List<string[]> filteredTable11490 = new List<string[]>();
+            foreach (string[] row in table11490)
+            {
+                if (row[0].Contains(search))
+                {
+                    filteredTable11490.Add(row);
+                }
+            }
+            dataGridView3.Rows.Clear();
+            if (filteredTable11490.Count > 0)
+            {
+                dataGridView3.RowCount = filteredTable11490.Count;
+                for (int i = 0; i < filteredTable11490.Count; i++)
+                {
+                    string[] row = filteredTable11490[i];
+                    dataGridView3.Rows[i].SetValues(row);
+                }
+            }
         }
     }
 }
